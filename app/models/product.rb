f0f503038b8b946720bd8452ed7676d5bce5d453
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-01 12:43:30 +0900

    ActiveHash（モデル修正・追加）

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :product_category
+  belongs_to :product_status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :days
 end
