3ecbfe5d978704b78a4c0c7f0aa491a0ff980e66
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-01 15:35:01 +0900

    商品出品ページを表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,12 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  
   def index
+    @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end  
+
 end
