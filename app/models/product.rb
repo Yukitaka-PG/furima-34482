a007ac5babf22798b97b1495429a68e2e0e99999
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-02 18:46:00 +0900

    指摘箇所修正１回目

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,7 +19,6 @@ class Product < ApplicationRecord
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :days_id, numericality: { other_than: 1 } 
     validates :price, numericality: {with: /\A[０−９]+\z/, message: "半角数字を使用してください"}
-    validates :user
   end
 
     validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"300以上9999999以下で入力してください"}
