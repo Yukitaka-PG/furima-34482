77b173fc42c251788de7c5b751f3a2c8ebfdf3c9
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-03 18:38:28 +0900

    指摘箇所修正1回目

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_product, only: [:edit, :show, :update]
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -20,15 +21,14 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+    
   end
 
   def edit
-    @product = Product.find(params[:id])
+    
   end
 
   def update
-    @product = Product.find(params[:id])
     @product.update(product_params)
     if @product.save
       redirect_to root_path
@@ -47,6 +47,10 @@ class ProductsController < ApplicationController
     unless current_user.id == @product.user.id
       redirect_to action: :index
     end
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 
 end
