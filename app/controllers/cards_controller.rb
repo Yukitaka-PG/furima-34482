5d94c2bea2c04ea6225a87cf2e38976f03d0c849
Yukitaka-PG <alliswellthatendwell0@gmail.com>
2021-03-06 21:25:33 +0900

    フォームの情報を保存

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,10 +1,21 @@
 class CardsController < ApplicationController
 
   def index
-    @card = Card.all
+    @destination_card = DestinationCard.new
   end
 
   def create
-    binding.pry
+    @destination_card = DestinationCard.new(destination_card_params)
+    if @destination_card.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def destination_card_params
+    params.permit(:destination_card).permit(:postal_code, :city, :address, :house_name, :phone_number, :burden_id, :prefecture_id, :product_id).merge(user_id: current_user.id)
   end
 end
